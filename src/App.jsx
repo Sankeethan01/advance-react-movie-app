9e94596b2d65903ba83cb63485d1b2d3921d89ea
Sankeethan01 <sankee26july@gmail.com>
2026-02-17 22:51:11 +0530

    fix(header): ensure code maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import header_image from './assets/images/header-image.jpg';
+import headerImage from './assets/images/header-image.jpg';
 import SearchBar from './components/SearchBar';
 
 const App = () => {
 
-  const [searchContext, setSearchContext] = useState("");
+  const [searchContext, setSearchContext] = useState('');
 
   return (
     <main>
       <div>
-        <img src={header_image} alt="header image" className='w-full h-64 object-cover' />
+        <img src={headerImage} alt="header image" className='w-full h-64 object-cover' />
         <h1 className="text-6xl font-bold text-center mt-4">Dive into the world of movies</h1>
         <h2 className="text-4xl font-bold text-center">Discover <span className="text-gradient">Ratings</span></h2>
         <h2 className="text-4xl font-bold text-center">Explore <span className="text-gradient">Genres</span></h2>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,18 @@
 import { BiSearchAlt } from "react-icons/bi"
 
 
-const SearchBar = ({ searchContext, setSearchContext }) => {
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
     <div className="search flex items-center justify-between relative">
         <input 
           type="text" 
           placeholder="Search for movies..." 
           className="w-full py-2" 
-          value={searchContext}
-          onChange={(e) => setSearchContext(e.target.value)}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          aria-label="Search for movies"
         />
-        <button className="absolute right-4 top-5 cursor-pointer"><BiSearchAlt className="w-6 h-6 text-gray-500" /></button>
+        <button  aria-label="Search" className="absolute right-4 top-5 cursor-pointer"><BiSearchAlt className="w-6 h-6 text-gray-500" /></button>
     </div>
   )
 }
