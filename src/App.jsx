2e6220512818baa26d0e0508a75facabf071eb13
Sankeethan01 <sankee26july@gmail.com>
2026-02-17 22:39:40 +0530

    refactor(header): makes proper content visibility

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
       <div>
         <img src={header_image} alt="header image" className='w-full h-64 object-cover' />
         <h1 className="text-6xl font-bold text-center mt-4">Dive into the world of movies</h1>
-        <h1 className="text-3xl font-bold text-center">Discover <span className="text-gradient">Ratings</span></h1>
-        <h1 className="text-3xl font-bold text-center">Explore <span className="text-gradient">Genres</span></h1>
-        <h1 className="text-3xl font-bold text-center">Find <span className="text-gradient">Top Movies</span></h1>
+        <h2 className="text-4xl font-bold text-center">Discover <span className="text-gradient">Ratings</span></h2>
+        <h2 className="text-4xl font-bold text-center">Explore <span className="text-gradient">Genres</span></h2>
+        <h2 className="text-4xl font-bold text-center">Find <span className="text-gradient">Top Movies</span></h2>
         <SearchBar searchContext={searchContext} setSearchContext={setSearchContext} />
       </div>
     </main>
